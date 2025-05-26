92a39e0f1d55ee3225a7fb15201b057b1e2a347f
Michael Fares Zietouny <MichaelZi@stlghana.com>
2025-05-26 13:04:08 +0000

    Errors on multi-lang-loop corrected

--- 6-multi_languages_loop.js
+++ 6-multi_languages_loop.js
@@ -1,5 +1,5 @@
 let lines = ["C is fun", "Python is cool", "JavaScript is amazing"];
 
-for(let i = 0; i < lines.length; i++){
+for (let i = 0; i < lines.length; i++){
     console.log(lines[i])
 };
