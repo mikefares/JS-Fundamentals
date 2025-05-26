e8fb74d3f27a2674fdbceaf52cfece5b31b1c61c
Michael Fares Zietouny <mickeyzietounyjnr@gmail.com>
2025-05-26 17:28:52 +0000

    Fixed factorial exercise error

--- 10-factorial.js
+++ 10-factorial.js
@@ -1,12 +1,12 @@
 const args = process.argv.slice(2);
 
-let n = args[0];
+let n = parseInt(args[0]);
 
 const factorial = function(n){
     if (n < 0){
         return "Undefined";
     }
-    else if (n == 0 || n == 1){
+    else if (isNaN(n) || n == 0 || n == 1){
         return 1;
     }
     return n * factorial(n - 1);
